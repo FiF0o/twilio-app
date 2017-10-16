bd8c3c3cec9a5626c26c50e000ef9fdcba15f4f6
Jon Lazarini <jonathan.lazarini@wipro.com>
2017-10-16 11:18:43 +0100

    add env variables

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+import os
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = ""
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
 # Your Auth Token from twilio.com/console
-auth_token  = ""
+auth_token  = os.environ["TWILIO_AUTH_TOKEN"]
+from_number = os.environ["TWILIO_NUMBER_FROM"]
+to_number = os.environ["TWILIO_NUMBER_TO"]
 
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="", 
-    from_="",
+    to=to_number, 
+    from_=from_number,
     body="Hello from Python!")
 
 print(message.sid)
